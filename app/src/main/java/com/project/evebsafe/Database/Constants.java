5c78ac4fc60ca05c1d336e0d181cdd1a74ebce02
Nuzhat Tabassum <ntabassum213@gmail.com>
2020-03-24 21:56:19 +0600

    version -0

---
+++ app/src/main/java/com/project/evebsafe/Database/Constants.java
@@ -0,0 +1,15 @@
+package com.project.evebsafe.Database;
+
+public class Constants {
+
+    public static final String APPS_PREFERENCE="evebsafe";
+    public static final String NAME="username_evebsafe";
+    public static final String PHONE="phone_evebsafe";
+    public static final String ADDRESS="address_evebsafe";
+    public static final String EMAIL="email_evebsafe";
+    public static final String LOCK_STATE="lockstate_evebsafe";
+    public static final String RUNNING_STATE="running_evebsafe";
+    public static final String PATTERN="pattern_evebsafe";
+    public static final String REGISTER="register_evebsafe";
+
+}
---
+++ app/src/main/java/com/project/evebsafe/Database/SharedPreference.java
@@ -0,0 +1,111 @@
+package com.project.evebsafe.Database;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class SharedPreference {
+    Context context;
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
+
+
+    public SharedPreference(Context context1) {
+        this.context=context1;
+    }
+
+    public void saveUser(String name,String phone,String address,String email ){
+        preferences=context.getSharedPreferences(Constants.APPS_PREFERENCE,Context.MODE_PRIVATE);
+        editor=preferences.edit();
+        editor.putString(Constants.NAME,name);
+        editor.putString(Constants.PHONE,phone);
+        editor.putString(Constants.ADDRESS,address);
+        editor.putString(Constants.EMAIL,email);
+        editor.commit();
+
+    }
+    public void runningState(boolean variable)
+    {
+        preferences=context.getSharedPreferences(Constants.APPS_PREFERENCE,Context.MODE_PRIVATE);
+        editor=preferences.edit();
+        editor.putBoolean(Constants.RUNNING_STATE,variable);
+        editor.commit();
+
+    }
+
+    public void lockState(boolean variable)
+    {
+        preferences=context.getSharedPreferences(Constants.APPS_PREFERENCE,Context.MODE_PRIVATE);
+        editor=preferences.edit();
+        editor.putBoolean(Constants.LOCK_STATE,variable);
+        editor.commit();
+
+    }
+
+    public void savePattern(String variable ) {
+        preferences = context.getSharedPreferences(Constants.APPS_PREFERENCE, Context.MODE_PRIVATE);
+        editor = preferences.edit();
+        editor.putString(Constants.PATTERN, variable);
+
+        editor.commit();
+    }
+
+    public boolean isRunning(){
+    preferences = context.getSharedPreferences(Constants.APPS_PREFERENCE, Context.MODE_PRIVATE);
+    return preferences.getBoolean(Constants.RUNNING_STATE,false);
+
+    }
+
+    public boolean isLocked(){
+        preferences = context.getSharedPreferences(Constants.APPS_PREFERENCE, Context.MODE_PRIVATE);
+        return preferences.getBoolean(Constants.LOCK_STATE,false);
+
+    }
+
+    public String getPattern(){
+        preferences = context.getSharedPreferences(Constants.APPS_PREFERENCE, Context.MODE_PRIVATE);
+        return preferences.getString(Constants.PATTERN,"");
+
+    }
+    public String getNames(){
+        preferences = context.getSharedPreferences(Constants.APPS_PREFERENCE, Context.MODE_PRIVATE);
+        return preferences.getString(Constants.NAME,"");
+
+    }
+
+
+    public String getAddress(){
+        preferences = context.getSharedPreferences(Constants.APPS_PREFERENCE, Context.MODE_PRIVATE);
+        return preferences.getString(Constants.ADDRESS,"");
+
+    }
+
+    public String getEmail(){
+        preferences = context.getSharedPreferences(Constants.APPS_PREFERENCE, Context.MODE_PRIVATE);
+        return preferences.getString(Constants.EMAIL,"");
+
+    }
+
+    public String getPhone()
+    {
+        preferences = context.getSharedPreferences(Constants.APPS_PREFERENCE, Context.MODE_PRIVATE);
+        return preferences.getString(Constants.PHONE,"");
+
+    }
+
+    public boolean isRegistered(){
+        String  name,phone;
+        name=getNames();
+        phone=getPhone();
+        if(name.isEmpty()&&phone.isEmpty())
+        {
+           return false;
+
+        }
+        else
+        {
+            return true;
+        }
+
+
+    }
+}
